4885f10e9df3c427ea4319d79c5a31216643d4ef
LorenzoLaManna3 <166180609+LorenzoLaManna3@users.noreply.github.com>
2024-04-29 22:44:10 +0200

    2.4
    -aggiunto js
    -aggiunto il modal per le immagini

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var modal = document.getElementById("idmodal");
+var images = document.getElementsByClassName('imgmodal')
+var modalImg = document.getElementById("img01");
+
+
+for (var i = 0; i < images.length; i++) {
+    var img = images[i];
+    img.onclick = function(evt){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+    }
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+// per chiudere
+span.onclick = function() { 
+  modal.style.display = "none";
+}
